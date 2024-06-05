3aeb21d87cb550f8ba8b05ba93dda759ea6f0e1b
Aluno <Aluno@10.85.198.10>
2024-06-05 16:37:05 -0300

    ShoppingCart

---
+++ src/NameMaker.java
@@ -0,0 +1,13 @@
+
+public class NameMaker {
+    
+    public static void main(String args[])
+    {
+        String firstName = "Barbara";
+        String middleName = "Fordiani";       
+        String lastName = "Moya";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println(fullName);
+    }
+    
+}
---
+++ src/ShoppingCart.java
@@ -0,0 +1,13 @@
+
+public class ShoppingCart {
+    public static void main (String[] args){
+        String custName = "Steve Smith";
+        String x = custName.substring(0, 5);
+        int idx1 = custName.indexOf('S');
+        String firstName = x;
+        int spaceIdx = idx1;
+        System.out.println(firstName);
+    }
+
+    
+}
